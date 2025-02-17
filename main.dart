1b8b8cc07da7d9aa61ba33caba4bbf1a101990bb
gleiseventura <gleisiele.ventura2016@outlook.com>
2025-02-17 03:31:59 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_planet_crud/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'CRUD de Planetas',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
 
